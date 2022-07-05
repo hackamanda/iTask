818194105a1573050930120d93bce251f8ebb4bf
hackamanda <hckamanda@gmail.com>
2022-07-05 14:00:42 -0300

    Adiciona primeiro commit

---
+++ iTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iTask
+//
+//  Created by Amanda Hack on 05/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
